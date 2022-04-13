259c81817309e462330a40cdc4230ea660e8616d
mwendwa-will <williammwendwa@gmail.com>
2022-04-13 13:34:43 +0300

    Additional

--- src/main/java/bazu/LevelEditorScene.java
+++ src/main/java/bazu/LevelEditorScene.java
@@ -1,86 +1,59 @@
 package bazu;
 
-
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL20;
 import render.Shader;
+import render.Texture;
 import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.glBindVertexArray;
-import static org.lwjgl.opengl.GL30.glGenVertexArrays;
-
+import static org.lwjgl.opengl.GL30.*;
 
 public class LevelEditorScene extends Scene {
 
-    private String vertexShaderSrc = "#version 330 core\n" +
-            "layout (location=0) in vec3 aPos;\n" +
-            "layout (location=1) in vec4 aColor;\n" +
-            "\n" +
-            "out vec4 fColor;\n" +
-            "\n" +
-            "void main()\n" +
-            "{\n" +
-            "    fColor = aColor;\n" +
-            "    gl_Position = vec4(aPos, 1.0);\n" +
-            "}";
-
-    private String fragmentShaderSrc ="#version 330 core\n" +
-            "\n" +
-            "in vec4 fColor;\n" +
-            "\n" +
-            "out vec4 color;\n" +
-            "\n" +
-            "void main()\n" +
-            "{\n" +
-            "    color = fColor;\n" +
-            "}";
     private int vertexID, fragmentID, shaderProgram;
 
     private float[] vertexArray = {
-        //position                  //color
-            100.5f, -0.5f, 0.0f,        1.0f,0.0f,0.0f,1.0f, // Bottom right
-            -50.5f, 100.5f, 0.0f,       0.0f,1.0f, 0.0f, 1.0f, //Top left
-            100.5f, 100.5f, 0.0f,       1.0f,0.0f,1.0f, 1.0f, //Top right
-            -50.5f, -50.5f, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f, // Bottom left
+            // position               // color                  // UV Coordinates
+            100f,   0f, 0.0f,       1.0f, 0.0f, 0.0f, 1.0f,     1, 1, // Bottom right 0
+            0f, 100f, 0.0f,       0.0f, 1.0f, 0.0f, 1.0f,     0, 0, // Top left     1
+            100f, 100f, 0.0f ,      1.0f, 0.0f, 1.0f, 1.0f,     1, 0, // Top right    2
+            0f,   0f, 0.0f,       1.0f, 1.0f, 0.0f, 1.0f,     0, 1  // Bottom left  3
     };
-    // IMPORTANT : Must be in counter-clockwise order
+
+    // IMPORTANT: Must be in counter-clockwise order
     private int[] elementArray = {
             /*
-                x       x
-
-
-                x       x
+                    x        x
+                    x        x
              */
-            2, 1, 0,// Top right triangle
-            0, 1, 3 // Bottom left triangle
+            2, 1, 0, // Top right triangle
+            0, 1, 3 // bottom left triangle
     };
 
     private int vaoID, vboID, eboID;
 
     private Shader defaultShader;
+    private Texture testTexture;
 
-    public LevelEditorScene(){
-        
+    public LevelEditorScene() {
 
     }
 
     @Override
-    public void init(){
-        this.camera = new Camera(new Vector2f());
-        defaultShader= new Shader("assets/shaders/default.glsl");
+    public void init() {
+        this.camera = new Camera(new Vector2f(-200, -300));
+        defaultShader = new Shader("assets/shaders/default.glsl");
         defaultShader.compile();
+        this.testTexture = new Texture("assets/images/testImage.jpg");
 
-
-
-
-
-        // =======================================================
-        // Generate VAO, VBO and EBO buffer objects and send to GPU
-        // =======================================================
+        // ============================================================
+        // Generate VAO, VBO, and EBO buffer objects, and send to GPU
+        // ============================================================
         vaoID = glGenVertexArrays();
         glBindVertexArray(vaoID);
 
@@ -98,47 +71,54 @@ public class LevelEditorScene extends Scene {
         elementBuffer.put(elementArray).flip();
 
         eboID = glGenBuffers();
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,eboID);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
 
-
         // Add the vertex attribute pointers
-        int positionSize = 3;
+        int positionsSize = 3;
         int colorSize = 4;
-        int floatSizeBytes = 4;
-        int vertexSizeBytes = (positionSize + colorSize) * floatSizeBytes ;
-        glVertexAttribPointer(0, positionSize, GL_FLOAT, false, vertexSizeBytes,0);
+        int uvSize = 2;
+        int vertexSizeBytes = (positionsSize + colorSize + uvSize) * Float.BYTES;
+        glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeBytes, 0);
         glEnableVertexAttribArray(0);
 
-        glVertexAttribPointer(1,colorSize,GL_FLOAT,false, vertexSizeBytes, positionSize * floatSizeBytes);
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * Float.BYTES);
+        glEnableVertexAttribArray(1);
 
+        glVertexAttribPointer(2, uvSize, GL_FLOAT, false, vertexSizeBytes, (positionsSize + colorSize) * Float.BYTES);
+        glEnableVertexAttribArray(2);
     }
 
     @Override
     public void update(float dt) {
-            camera.position.x -= dt *  50.0f;
-            defaultShader.use();
-            defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
-            defaultShader.uploadMat4f("uView", camera.getViewMatrix());
-            defaultShader.uploadFloat("uTime", Time.getTime());
-        // Bind the VAO that we are using
+//        camera.position.x -= dt * 50.0f;
+//        camera.position.y -= dt * 20.0f;
+
+        defaultShader.use();
+
+        // Upload texture to shader
+        defaultShader.uploadTexture("TEX_SAMPLER", 0);
+        glActiveTexture(GL_TEXTURE0);
+        testTexture.bind();
+
+        defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
+        defaultShader.uploadMat4f("uView", camera.getViewMatrix());
+        defaultShader.uploadFloat("uTime", Time.getTime());
+        // Bind the VAO that we're using
         glBindVertexArray(vaoID);
 
-        //Enable the vertex attribute pointers
+        // Enable the vertex attribute pointers
         glEnableVertexAttribArray(0);
         glEnableVertexAttribArray(1);
 
-        glDrawElements(GL_TRIANGLES,elementArray.length,GL_UNSIGNED_INT,0);
+        glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
 
-        //Unbind everything
+        // Unbind everything
         glDisableVertexAttribArray(0);
         glDisableVertexAttribArray(1);
 
         glBindVertexArray(0);
 
         defaultShader.detach();
-
-
-
     }
 }
--- src/main/java/render/Shader.java
+++ src/main/java/render/Shader.java
@@ -3,6 +3,7 @@ package render;
 import org.joml.*;
 import org.lwjgl.BufferUtils;
 
+import javax.print.DocFlavor;
 import java.io.IOException;
 import java.nio.FloatBuffer;
 import java.nio.file.Files;
@@ -52,54 +53,49 @@ public class Shader {
             } else {
                 throw new IOException("Unexpected token '" + secondPattern + "'");
             }
-
-        } catch (IOException e) {
+        } catch(IOException e) {
             e.printStackTrace();
             assert false : "Error: Could not open file for shader: '" + filepath + "'";
         }
-
-        System.out.println(vertexSource);
-        System.out.println(fragmentSource);
-
     }
 
     public void compile() {
-        //==============================================
-        // Compile and Link shaders
-        //==============================================
-
+        // ============================================================
+        // Compile and link shaders
+        // ============================================================
         int vertexID, fragmentID;
-        // First load and compile vertex shader
+
+        // First load and compile the vertex shader
         vertexID = glCreateShader(GL_VERTEX_SHADER);
-        //Pass ShaderSRC to GPU
+        // Pass the shader source to the GPU
         glShaderSource(vertexID, vertexSource);
         glCompileShader(vertexID);
 
-        //Check for Errors
+        // Check for errors in compilation
         int success = glGetShaderi(vertexID, GL_COMPILE_STATUS);
         if (success == GL_FALSE) {
             int len = glGetShaderi(vertexID, GL_INFO_LOG_LENGTH);
-            System.out.println("Error: '" + filepath + "'\n\tVertex shader compilation failed.");
+            System.out.println("ERROR: '" + filepath + "'\n\tVertex shader compilation failed.");
             System.out.println(glGetShaderInfoLog(vertexID, len));
             assert false : "";
         }
 
-        // First load and compile fragment shader
+        // First load and compile the vertex shader
         fragmentID = glCreateShader(GL_FRAGMENT_SHADER);
-        //Pass ShaderSRC to GPU
+        // Pass the shader source to the GPU
         glShaderSource(fragmentID, fragmentSource);
         glCompileShader(fragmentID);
 
-        //Check for Errors
+        // Check for errors in compilation
         success = glGetShaderi(fragmentID, GL_COMPILE_STATUS);
         if (success == GL_FALSE) {
             int len = glGetShaderi(fragmentID, GL_INFO_LOG_LENGTH);
-            System.out.println("Error: '" + filepath + "'\n\tFragment shader compilation failed.");
+            System.out.println("ERROR: '" + filepath + "'\n\tFragment shader compilation failed.");
             System.out.println(glGetShaderInfoLog(fragmentID, len));
             assert false : "";
         }
 
-        // Link Shaders and Check for Errors
+        // Link shaders and check for errors
         shaderProgramID = glCreateProgram();
         glAttachShader(shaderProgramID, vertexID);
         glAttachShader(shaderProgramID, fragmentID);
@@ -113,16 +109,14 @@ public class Shader {
             System.out.println(glGetProgramInfoLog(shaderProgramID, len));
             assert false : "";
         }
-
     }
 
     public void use() {
-        if (!beingUsed){
-            //bind shader program
+        if (!beingUsed) {
+            // Bind shader program
             glUseProgram(shaderProgramID);
             beingUsed = true;
         }
-
     }
 
     public void detach() {
@@ -130,50 +124,55 @@ public class Shader {
         beingUsed = false;
     }
 
-    public  void uploadMat4f(String varName, Matrix4f mat4){
-        int varLocation = glGetUniformLocation(shaderProgramID,varName);
+    public void uploadMat4f(String varName, Matrix4f mat4) {
+        int varLocation = glGetUniformLocation(shaderProgramID, varName);
         use();
         FloatBuffer matBuffer = BufferUtils.createFloatBuffer(16);
         mat4.get(matBuffer);
-        glUniformMatrix4fv(varLocation, false, matBuffer );
+        glUniformMatrix4fv(varLocation, false, matBuffer);
     }
 
-    public  void uploadMat3f(String varName, Matrix3f mat3){
-        int varLocation = glGetUniformLocation(shaderProgramID,varName);
+    public void uploadMat3f(String varName, Matrix3f mat3) {
+        int varLocation = glGetUniformLocation(shaderProgramID, varName);
         use();
         FloatBuffer matBuffer = BufferUtils.createFloatBuffer(9);
         mat3.get(matBuffer);
-        glUniformMatrix3fv(varLocation, false, matBuffer );
+        glUniformMatrix3fv(varLocation, false, matBuffer);
     }
 
-    public void uploadVec4f(String varName, Vector4f vec){
+    public void uploadVec4f(String varName, Vector4f vec) {
         int varLocation = glGetUniformLocation(shaderProgramID, varName);
         use();
-        glUniform4f(varLocation,vec.x,vec.y,vec.z,vec.w);
+        glUniform4f(varLocation, vec.x, vec.y, vec.z, vec.w);
     }
 
-    public void uploadVec3f(String varName, Vector3f vec){
+    public void uploadVec3f(String varName, Vector3f vec) {
         int varLocation = glGetUniformLocation(shaderProgramID, varName);
         use();
-        glUniform3f(varLocation,vec.x,vec.y,vec.z);
+        glUniform3f(varLocation, vec.x, vec.y, vec.z);
     }
 
-    public void uploadVec2f(String varName, Vector2f vec){
+    public void uploadVec2f(String varName, Vector2f vec) {
         int varLocation = glGetUniformLocation(shaderProgramID, varName);
         use();
-        glUniform2f(varLocation,vec.x,vec.y);
+        glUniform2f(varLocation, vec.x, vec.y);
     }
 
     public void uploadFloat(String varName, float val) {
         int varLocation = glGetUniformLocation(shaderProgramID, varName);
         use();
-        glUniform1f(varLocation,val);
+        glUniform1f(varLocation, val);
     }
 
-    public void uploadInt(String varName, int val){
+    public void uploadInt(String varName, int val) {
         int varLocation = glGetUniformLocation(shaderProgramID, varName);
         use();
-        glUniform1i(varLocation,val);
+        glUniform1i(varLocation, val);
     }
 
+    public void uploadTexture(String varName, int slot) {
+        int varLocation = glGetUniformLocation(shaderProgramID, varName);
+        use();
+        glUniform1i(varLocation, slot);
+    }
 }
