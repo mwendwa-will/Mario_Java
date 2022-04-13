4504bd24b5236f6b76729bfa963a78aaf694658f
mwendwa-will <williammwendwa@gmail.com>
2022-04-13 11:43:03 +0300

    Initial Mario files

--- src/main/java/bazu/LevelEditorScene.java
+++ src/main/java/bazu/LevelEditorScene.java
@@ -4,6 +4,7 @@ package bazu;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import render.Shader;
+import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -119,6 +120,7 @@ public class LevelEditorScene extends Scene {
             defaultShader.use();
             defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
             defaultShader.uploadMat4f("uView", camera.getViewMatrix());
+            defaultShader.uploadFloat("uTime", Time.getTime());
         // Bind the VAO that we are using
         glBindVertexArray(vaoID);
 
